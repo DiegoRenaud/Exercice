0ee892ed6782bc72a5646e3ae7c544fb7c99a694
mchabrol <marion.chabrol@ensae.fr>
2022-12-18 12:26:43 +0000

    mise en forme 2

--- fonctions.py
+++ fonctions.py
@@ -2,6 +2,8 @@ import requests
 import pandas as pd
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #importation des donnees
 def importer(url):
@@ -10,13 +12,23 @@ def importer(url):
     df = pd.DataFrame(wb)
     return(df)
 
+#régression linéaire
 def regression(x, y):
     x_reg = sm.add_constant(x)
     y_reg = y.values.ravel()
     xTrain, xTest, yTrain, yTest = train_test_split(x_reg, y_reg, test_size=0.2)
     return([sm.OLS(yTrain, xTrain).fit(), xTrain, xTest, yTrain, yTest])
 
+#passage des pourcentages aux niveaux
 def niveau(df, nb_retards, causes):
     for cause in causes[:-1] :
         df[cause] = df['prct_'+cause]/100*nb_retards
     return(df)
+
+#visualisation de l'erreur dans le cadre de la régression
+def visu_erreur(yPred, yTest):
+    tempdf = pd.DataFrame({"prediction": yPred, "observed": yTest,
+                       "epsilon": yTest - yPred})
+    fig = plt.figure()
+    g = sns.scatterplot(data = tempdf, x = "observed", y = "epsilon")
+    g.axhline(0, color = "red")
