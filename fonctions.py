384766e05cbac54dc049b9bd76889f996caabb28
mchabrol <marion.chabrol@ensae.fr>
2022-12-19 23:34:36 +0000

    maj fonctions

--- fonctions.py
+++ fonctions.py
@@ -5,7 +5,9 @@ import numpy as np
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 import seaborn as sns
+import sklearn.metrics
 
 #importation des donnees
 def importer(url):
@@ -18,12 +20,11 @@ def importer(url):
 def regression(x, y):
     x_reg = sm.add_constant(x)
     y_reg = y.values.ravel()
-    xTrain, xTest, yTrain, yTest = train_test_split(x_reg, y_reg, test_size=0.2)
-    return([sm.OLS(yTrain, xTrain).fit(), xTrain, xTest, yTrain, yTest])
+    return(sm.OLS(y_reg, x_reg).fit())
 
 #passage des pourcentages aux niveaux
 def niveau(df, nb_retards, causes):
-    for cause in causes[:-1] :
+    for cause in causes :
         df[cause] = df['prct_'+cause]/100*nb_retards
     return(df)
 
@@ -34,3 +35,30 @@ def visu_erreur(yPred, yTest):
     fig = plt.figure()
     g = sns.scatterplot(data = tempdf, x = "observed", y = "epsilon")
     g.axhline(0, color = "red")
+
+def prediction(x, y, afficher = True):
+    #séparation des données en 2 échantillon 
+    X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+    
+    #régression et prédiction
+    ols = LinearRegression().fit(X_train, y_train)
+    y_pred = ols.predict(X_test)
+    
+    #calcul des métriques
+    rmse = sklearn.metrics.mean_squared_error(y_test, y_pred, squared = False)
+    rsq = sklearn.metrics.r2_score(y_test, y_pred) 
+    
+    #nuage de points des valeurs observées
+    tempdf = pd.DataFrame({"prediction": y_pred, "observed": y_test,
+                           "error": y_test - y_pred})
+    if afficher == True : 
+        print("intercept : ",ols.intercept_)
+        print("coeffs : ", ols.coef_)
+        print("rsq : ", rsq)
+        print("rmse : ", rmse)
+
+        fig = plt.figure()
+        g = sns.scatterplot(data = tempdf, x = "observed", y = "error")
+        g.axhline(0, color = "red")
+    
+    return([X_train, y_train])
